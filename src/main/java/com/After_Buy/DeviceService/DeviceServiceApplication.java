f1028d021274c44b6edfcb4b41e9ebc7c7253fab
junhyeok020630 <skylove2424@gmail.com>
2026-03-25 09:47:11 +0900

    feat: 신규 프로젝트 생성

---
+++ src/main/java/com/After_Buy/DeviceService/DeviceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.After_Buy.DeviceService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceServiceApplication.class, args);
+	}
+
+}
