36cb357d35c714dd291bd9d39af3a2c4bc07e81b
junhyeok020630 <skylove2424@gmail.com>
2026-04-06 18:30:29 +0900

    fix: 빌드 테스틍용 설정 추가

--- src/test/java/com/After_Buy/DeviceService/DeviceServiceApplicationTests.java
+++ src/test/java/com/After_Buy/DeviceService/DeviceServiceApplicationTests.java
@@ -3,7 +3,10 @@ package com.After_Buy.DeviceService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.ActiveProfiles;
+
 @SpringBootTest
+@ActiveProfiles("test")
 class DeviceServiceApplicationTests {
 
 	@Test
